0f19fc04e51a2bcd85ef57364a4b627d85f53220
hiyorun <hiyorrun@e.email>
2024-01-03 23:07:20 +0700

    feat: generate colour from image

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
+#include <cstdint>
+#define MAGICKCORE_HDRI_ENABLE 1
+#include "cpp/quantize/celebi.h"
+#include "cpp/quantize/wsmeans.h"
+#include "cpp/score/score.h"
+#include "cpp/utils/utils.h"
 #include "output/output.hpp"
+#include <Magick++.h>
+#include <vector>
 
 #include <iostream>
 #include <iterator>
 #include <ostream>
 #include <sstream>
 
+using namespace Magick;
 using namespace material_color_utilities;
 using namespace std;
 
@@ -21,19 +30,44 @@ Argb stringToArgb(const string &str) {
 
 void help() {
   cout << "Usage: iroha [OPTIONS]\n"
-       << "Options:\n"
-       << "  -c, --colour <value>\t\tDeclare a color using ARGB format (e.g., "
+       << "\n"
+       << "Colour passing methods:\n"
+       << "  -c, --colour <value>\t\tDeclare a colour using ARGB format (e.g., "
           "ff902922)\n"
+       << "  -m, --image <path>\t\tPath to image\n"
+       << "\n"
+       << "Outputs:\n"
+       << "  -i, --inja <template-path>\tPath to Inja template for the "
+          "palette. This will require -o\n"
+       << "  -o, --output <path>\t\tOutput path\n"
+       << "  -j, --json\t\t\tPrint JSON representation of the palette\n"
+       << "\n"
+       << "Options:\n"
        << "  -d, --dark-mode\t\tEnable dark mode\n"
        << "  -h, --help\t\t\tDisplay this help message\n"
-       << "  -i, --inja <template-path>\tPrint Inja template for the palette\n"
-       << "  -j, --json\t\t\tPrint JSON representation of the palette\n"
-       << "  -i, --inja <template-path>\tPrint Inja template for the palette\n"
        << "\n"
        << "Examples:\n"
        << "  iroha -j --colour ff902922\n"
        << "  iroha -d -j -c ff902922\n"
-       << "  iroha -i template.css -c ff902922\n";
+       << "  iroha -i template.css -o output.css -c ff902922\n"
+       << "  iroha -m poisonous-red-fluff.png -i template.css -o output.css\n";
+}
+
+vector<Argb> magicc(string path) {
+  vector<Argb> colours;
+  Image image(path);
+  image.resize(Magick::Geometry(256, 256));
+  for (int i = 0; i < image.columns(); i++) {
+    for (int j = 0; j < image.rows(); j++) {
+      ColorRGB pixel = image.pixelColor(i, j);
+      stringstream ss;
+      ss << hex
+         << (int(pixel.alpha() * 255) << 24 | int(pixel.red() * 255) << 16 |
+             int(pixel.green() * 255) << 8 | int(pixel.blue() * 255));
+      colours.push_back(stringToArgb(ss.str()));
+    }
+  }
+  return colours;
 }
 
 int main(int argc, char *argv[]) {
@@ -41,6 +75,8 @@ int main(int argc, char *argv[]) {
     help();
     return 1;
   }
+
+  InitializeMagick(*argv);
 
   Argb argb;
 
@@ -67,6 +103,25 @@ int main(int argc, char *argv[]) {
           inja.printInja(palette);
         });
       }
+      flag++;
+    } else if (*flag == "-m" || *flag == "--image") {
+      if (next(flag) == args.end()) {
+        help();
+        return 1;
+      }
+      vector<Argb> argbVec = magicc(next(flag)->c_str());
+      QuantizerResult quantizerRes = QuantizeCelebi(argbVec, uint32_t(128));
+
+      std::map<Argb, uint32_t> convertedMap;
+
+      for (const auto &pair : quantizerRes.color_to_count) {
+        // Assuming that the values are within the valid range of uint32_t
+        convertedMap[pair.first] = static_cast<uint32_t>(pair.second);
+      }
+
+      vector<Argb> colours = RankedSuggestions(convertedMap);
+
+      argb = colours[0];
       flag++;
     } else if (*flag == "-j" || *flag == "--json") {
       outOps.push_back([](Palette &palette) { printJson(palette); });
